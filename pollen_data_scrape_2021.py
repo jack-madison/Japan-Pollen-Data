d79a5ec6ab983ab77c7cfbcc5a66a84e0c6324e8
Jack Madison <madison@sfu.ca>
2021-10-19 14:23:12 -0700

    update code

--- pollen_data_scrape_2021.py
+++ pollen_data_scrape_2021.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import json
 import requests
 from datetime import datetime
@@ -49,21 +50,47 @@ for x in prefectures:
 # Tell the user that the for loop is complete
 print("For loop complete!")
 
-# Create a dataframe with the unique station names
-stations = pd.DataFrame(pollen_data_2021['Measuring station name'].unique())
-stations['s_point'] = stations.index + 1
-stations.columns = ['Measuring station name', 's_point']
+# Open the excel file that contains the s_point data
+stations = pd.read_excel('./s_point.xlsx')
+stations.columns = ['s_point', 'Measuring station name']
 
+# Replace the string '\u3000' in one of the station names as it causes errors
+pollen_data_2021['Measuring station name'] = pollen_data_2021['Measuring station name'].str.replace('\u3000', ' ')
+
+# Merge the pollen data for 2021 with the station numbers following the assigned values used when collecting the 2019 and 2020 data
 pollen_data_2021 = pd.merge(pollen_data_2021, stations, how = 'left', on = 'Measuring station name')
 
+# Set the conditions to create the region variable
+conditions = [
+    (pollen_data_2021['prefecture'] == 'Hokkaido'),
+    (pollen_data_2021['prefecture'] == 'Aomori') | (pollen_data_2021['prefecture'] == 'Iwate') | (pollen_data_2021['prefecture'] == 'Miyagi') | (pollen_data_2021['prefecture'] == 'Akita') | (pollen_data_2021['prefecture'] == 'Yamagata') | (pollen_data_2021['prefecture'] == 'Fukushima'),
+    (pollen_data_2021['prefecture'] == 'Ibaraki') | (pollen_data_2021['prefecture'] == 'Tochigi') | (pollen_data_2021['prefecture'] == 'Gunma') | (pollen_data_2021['prefecture'] == 'Saitama') | (pollen_data_2021['prefecture'] == 'Chiba') | (pollen_data_2021['prefecture'] == 'Tokyo') | (pollen_data_2021['prefecture'] == 'Kanagawa'),
+    (pollen_data_2021['prefecture'] == 'Niigata') | (pollen_data_2021['prefecture'] == 'Toyama') | (pollen_data_2021['prefecture'] == 'Ishikawa') | (pollen_data_2021['prefecture'] == 'Fukui') | (pollen_data_2021['prefecture'] == 'Yamanashi') | (pollen_data_2021['prefecture'] == 'Nagano') | (pollen_data_2021['prefecture'] == 'Gifu') | (pollen_data_2021['prefecture'] == 'Shizuoka') | (pollen_data_2021['prefecture'] == 'Aichi'),
+    (pollen_data_2021['prefecture'] == 'Mie') | (pollen_data_2021['prefecture'] == 'Shiga') | (pollen_data_2021['prefecture'] == 'Kyoto') | (pollen_data_2021['prefecture'] ==  'Osaka') | (pollen_data_2021['prefecture'] == 'Hyogo') | (pollen_data_2021['prefecture'] == 'Nara') | (pollen_data_2021['prefecture'] == 'Wakayama'),
+    (pollen_data_2021['prefecture'] == 'Tottori') | (pollen_data_2021['prefecture'] == 'Shimane') | (pollen_data_2021['prefecture'] == 'Okayama') | (pollen_data_2021['prefecture'] == 'Hiroshima') | (pollen_data_2021['prefecture'] == 'Yamaguchi'),
+    (pollen_data_2021['prefecture'] == 'Tokushima') | (pollen_data_2021['prefecture'] == 'Kagawa') | (pollen_data_2021['prefecture'] == 'Ehime') | (pollen_data_2021['prefecture'] == 'Kochi'),
+    (pollen_data_2021['prefecture'] == 'Fukuoka') | (pollen_data_2021['prefecture'] == 'Saga') | (pollen_data_2021['prefecture'] == 'Nagasaki') | (pollen_data_2021['prefecture'] == 'Kumamoto') | (pollen_data_2021['prefecture'] == 'Oita') | (pollen_data_2021['prefecture'] == 'Miyazaki') | (pollen_data_2021['prefecture'] == 'Kagoshima')
+    ]
+
+# Set the values for the region variable
+values = ['Hokkaido', 'Tohoku', 'Kanto', 'Chubu', 'Kansai', 'Chugoku', 'Shikoku', 'Kyushu']
+
+# Create the region variable based on the conditions and values
+pollen_data_2021['region'] = np.select(conditions, values)    
+
+# Format the date to be in line with the 2019 and 2020 data that has already been collected
 pollen_data_2021['date'] = pd.to_datetime(pollen_data_2021['Measurement date'])
 pollen_data_2021['year'] = pollen_data_2021['date'].dt.year
 pollen_data_2021['month'] = pollen_data_2021['date'].dt.month
 pollen_data_2021['day'] = pollen_data_2021['date'].dt.day
 
+# Change the data type of the hour and s_point variables
 pollen_data_2021['hour'] = pollen_data_2021['hour'].astype(int)
+pollen_data_2021['s_point'] = pollen_data_2021['s_point'].astype(int)
 
+# Remove unneeded columns
 pollen_data_2021 = pollen_data_2021[['s_point', 'date', 'year', 'month', 'day', 'hour', 'pollen', 'rainfall', 'temperature', 
-'wind_direction', 'wind_speed', 'prefecture']]
+'wind_direction', 'wind_speed', 'region']]
 
-pollen_data_2021.to_csv('./pollen_data2021.csv')
+# Save the data as a csv to be used later
+pollen_data_2021.to_csv('./pollen_data2021.csv', index=False)
